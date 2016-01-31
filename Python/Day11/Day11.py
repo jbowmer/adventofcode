79b11634d2e63b39bbbdbba7fabc5b67d77c2c9a
Jake <jakebowmer@gmail.com>
2016-01-31 16:58:18 +1100

    Assorted Days

--- Python/Day11/Day11.py
+++ Python/Day11/Day11.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan  5 20:51:41 2016
+Created on Sat Jan 30 20:33:38 2016
 
 @author: Jake
 """
 
-#Advent of Code, Day11
+#Day11 Advent Of Code:
+
+#the last five letters of the string should be of the form aabcc
+
+input_string = 'hxbxwxba'
+
+#This one is solvable by hand - hxbxwxba -> hxbxxyzz -> hxcaabcc
